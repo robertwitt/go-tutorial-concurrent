a6ce4760922a3cfc9d9485a722d3c7f6ccb646bf
Robert Witt <robert.witt@sap.com>
2020-07-03 13:06:04 +0200

    Use channels

--- main.go
+++ main.go
@@ -13,23 +13,42 @@ var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 func main() {
 	wg := &sync.WaitGroup{}
 	m := &sync.RWMutex{}
+	cacheCh := make(chan Book)
+	dbCh := make(chan Book)
+
 	for i := 0; i < 10; i++ {
 		id := rnd.Intn(10) + 1
 		wg.Add(2)
-		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex, ch chan<- Book) {
 			if b, ok := queryCache(id, m); ok {
-				fmt.Println("from cache")
-				fmt.Println(b)
+				ch <- b
 			}
 			wg.Done()
-		}(id, wg, m)
-		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+		}(id, wg, m, cacheCh)
+
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex, ch chan<- Book) {
 			if b, ok := queryDatabase(id, m); ok {
+				m.Lock()
+				cache[id] = b
+				m.Unlock()
+				ch <- b
+			}
+			wg.Done()
+		}(id, wg, m, dbCh)
+
+		go func(cacheCh, dbCh <-chan Book) {
+			select {
+			case b := <-cacheCh:
+				fmt.Println("from cache")
+				fmt.Println(b)
+				<-dbCh
+			case b := <-dbCh:
 				fmt.Println("from database")
 				fmt.Println(b)
 			}
-			wg.Done()
-		}(id, wg, m)
+		}(cacheCh, dbCh)
+
 		time.Sleep(150 * time.Millisecond)
 	}
 
@@ -47,9 +66,6 @@ func queryDatabase(id int, m *sync.RWMutex) (Book, bool) {
 	time.Sleep(100 * time.Millisecond)
 	for _, b := range books {
 		if b.ID == id {
-			m.Lock()
-			cache[id] = b
-			m.Unlock()
 			return b, true
 		}
 	}
